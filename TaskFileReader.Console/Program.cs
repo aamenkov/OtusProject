ef0ae46ec5fe240fb22a6caa6c367c6adb4b8ad8
aamenkov <aamenkov@mail.ru>
2024-01-29 22:16:49 +0300

    Начал выполнение 6го задания

---
+++ TaskFileReader.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+internal class Program
+{
+    /// <summary>
+    /// Сервис для конфигурации 
+    /// </summary>
+   // private static ConfigService _configService;
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world!");
+    }
+}
