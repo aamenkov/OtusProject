e9300ddd5df76ee6f07729734f94d1e4dca7663c
aamenkov <aamenkov@mail.ru>
2024-02-06 00:11:27 +0300

    Добавлено 6-ое задание.

--- TaskFileReader.Console/Program.cs
+++ TaskFileReader.Console/Program.cs
@@ -1,16 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using System.Diagnostics;
 
 internal class Program
 {
-    /// <summary>
-    /// Сервис для конфигурации 
-    /// </summary>
-   // private static ConfigService _configService;
 
-
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello world!");
+        // Получаем исходную директорию
+        string projectPath = Directory.GetCurrentDirectory();
+        string folderPath = Path.Combine(projectPath, "TargetFolder");
+
+        // Первый способ
+        await ProcessFilesAsync(folderPath);
+
+        // Второй способ
+        await ProcessFilesParalellForEachAsync(folderPath);      
+    }
+
+    /// <summary>
+    /// Способ с массивом из Task
+    /// </summary>
+    /// <param name="folderPath"></param>
+    /// <returns></returns>
+    public static async Task ProcessFilesAsync(string folderPath)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        string[] fileEntries = Directory.GetFiles(folderPath);
+        Task[] tasks = new Task[fileEntries.Length];
+
+        for (int i = 0; i < fileEntries.Length ; i++)
+        {
+            int index = i;
+            tasks[i] = Task.Run(() =>
+            {
+                Console.WriteLine($"Поток {index} запущен!");
+                string content = File.ReadAllText(fileEntries[index]);
+                int spaceCount = content.Split(' ').Length - 1;
+                Console.WriteLine($"Поток {index} закончил работу! Файл: " + fileEntries[index] + ", пробелов: " + spaceCount);
+            });
+        }
+
+        await Task.WhenAll(tasks);
+        stopwatch.Stop();
+        Console.WriteLine("Время выполнения метода 'Способ с массивом из Task': " + stopwatch.ElapsedMilliseconds + " миллисекунд");
+    }
+
+
+    /// <summary>
+    /// Способ с ParalelForEach
+    /// </summary>
+    /// <param name="folderPath"></param>
+    /// <returns></returns>
+    public static async Task ProcessFilesParalellForEachAsync(string folderPath)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        await Task.Run(() => 
+        { 
+            string[] fileEntries = Directory.GetFiles(folderPath);
+            Parallel.ForEach(fileEntries, (fileEntry, parallelLoopState, index) =>
+            {
+                Console.WriteLine($"Поток {index} запущен!");
+                string content = File.ReadAllText(fileEntry);
+                int spaceCount = content.Split(' ').Length - 1;
+                Console.WriteLine($"Поток {index} закончил работу! Файл: " + fileEntry + ", пробелов: " + spaceCount);
+            });
+        }
+        );
+        stopwatch.Stop();
+        Console.WriteLine("Время выполнения метода 'Способ с ParalelForEach': " + stopwatch.ElapsedMilliseconds + " миллисекунд");
     }
 }
